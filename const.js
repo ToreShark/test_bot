fb79920c8a877e20b59e56085e4b8971ca5bfe56
MacBook <macbook@MacBook-Air-MacBook.local>
2022-10-11 13:15:10 +0600

    5

--- const.js
+++ const.js
@@ -3,5 +3,8 @@ const command = `
 /help - Помощь
 /process - Документы для судов и гос органов
 `
-
+const text1 = `
+В этой папке необходимые документы при бездействий судебного исполнителя <a href="https://drive.google.com/drive/folders/1yv04Va5Lu5qsdSytLU3AWoiHEI-Jd21o?usp=sharing">Перейти</a>
+`
 module.exports.command = command;
+module.exports.text1 = text1;
--- index.js
+++ index.js
@@ -13,10 +13,11 @@ bot.command('process', async (ctx) => {
         await ctx.replyWithHTML(`<b>Документы</b>`, Markup.inlineKeyboard(
             [
                 [Markup.button.callback(`Обжалование бездействие ЧСИ`, `btn_1`)],
-                [Markup.button.callback(`Привлечение к уголовной ответственности за неуплату алиментов`, `btn_2`)],
+                [Markup.button.callback(`Уголовная ответственность за неуплату алиментов`, `btn_2`)],
                 [Markup.button.callback(`Увеличение размеров алиментов`, `btn_3`)],
                 [Markup.button.callback(`Иск о взыскании алиментов`, `btn_4`)],
                 [Markup.button.callback(`Вся информация по алиментам`, `btn_5`)],
+                [Markup.button.callback(`Отправить результаты мне`, `btn_6`)],
             ]
             ))
     } catch (e) {
@@ -25,15 +26,23 @@ bot.command('process', async (ctx) => {
     
 })
 
-bot.action('btn_1', async (ctx) => {
-    try {
-        await ctx.replyWithHTML('Обработка кнопки 1', {
-            disable_web_page_preview: true
-        })
-    } catch (e) {
-        console.error(e)
-    }
-})
+function addActionBot(name, src, text) {
+    bot.action(name, async (ctx) => {
+        try {
+            if (src !== false) {
+                await ctx.replyWithPhoto({
+                    source: src
+                })
+            }
+            await ctx.replyWithHTML(text, {
+                disable_web_page_preview: true
+            })
+        } catch (e) {
+            console.error(e)
+        }
+    })
+}
+addActionBot('btn_1', './image/1.jpeg', text.text1)
 
 
 bot.launch();
